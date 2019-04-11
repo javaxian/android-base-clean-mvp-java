d9e518a6a98f13748fca4d2d6240821dc7f8c3a6
javaxian <import.javax.ian@gmail.com>
2019-04-10 22:47:59 -0500

    First commit. Initial architecture layers and packages

---
+++ app/src/main/java/com/javaxian/cleanarchitecture/mvpjava/presentation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.javaxian.cleanarchitecture.mvpjava.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.javaxian.cleanarchitecture.mvpjava.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
